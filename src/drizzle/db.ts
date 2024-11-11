6b6e70a30637bafdb600c6cefbaf5ac9a45915b5
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-11-12 00:50:29 +0530

    events page done

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema'
 
 const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql });
+export const db = drizzle(sql, {schema});
 
-const result = await db.execute('select 1');
