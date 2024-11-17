701010d1c9bc324df855dc86588d91c32fabbb92
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-11-17 15:29:15 +0530

    booking form

--- src/server/googleCalendar.ts
+++ src/server/googleCalendar.ts
@@ -40,23 +40,73 @@ export async function getCalendarEventTimes(
   )
 }
 
-async function getOAuthClient(clerkUserId: string) {
-    const token = await (await clerkClient()).users.getUserOauthAccessToken(
-      clerkUserId,
-      "oauth_google"
-    )
-  
-    if (token.data.length === 0 || token.data[0].token == null) {
-      return
-    }
-  
-    const client = new google.auth.OAuth2(
-      process.env.GOOGLE_OAUTH_CLIENT_ID,
-      process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-      process.env.GOOGLE_OAUTH_REDIRECT_URL
-    )
-  
-    client.setCredentials({ access_token: token.data[0].token })
-  
-    return client
+export async function createCalendarEvent({
+  clerkUserId,
+  guestName,
+  guestEmail,
+  startTime,
+  guestNotes,
+  durationInMinutes,
+  eventName,
+}: {
+  clerkUserId: string
+  guestName: string
+  guestEmail: string
+  startTime: Date
+  guestNotes?: string | null
+  durationInMinutes: number
+  eventName: string
+}) {
+  const oAuthClient = await getOAuthClient(clerkUserId)
+  const calendarUser = await (await clerkClient()).users.getUser(clerkUserId)
+  if (calendarUser.primaryEmailAddress == null) {
+    throw new Error("Clerk user has no email")
   }
+
+  const calendarEvent = await google.calendar("v3").events.insert({
+    calendarId: "primary",
+    auth: oAuthClient,
+    sendUpdates: "all",
+    requestBody: {
+      attendees: [
+        { email: guestEmail, displayName: guestName },
+        {
+          email: calendarUser.primaryEmailAddress.emailAddress,
+          displayName: calendarUser.fullName,
+          responseStatus: "accepted",
+        },
+      ],
+      description: guestNotes ? `Additional Details: ${guestNotes}` : undefined,
+      start: {
+        dateTime: startTime.toISOString(),
+      },
+      end: {
+        dateTime: addMinutes(startTime, durationInMinutes).toISOString(),
+      },
+      summary: `${guestName} + ${calendarUser.fullName}: ${eventName}`,
+    },
+  })
+
+  return calendarEvent.data
+}
+
+async function getOAuthClient(clerkUserId: string) {
+  const token = await (await clerkClient()).users.getUserOauthAccessToken(
+    clerkUserId,
+    "oauth_google"
+  )
+
+  if (token.data.length === 0 || token.data[0].token == null) {
+    return
+  }
+
+  const client = new google.auth.OAuth2(
+    process.env.GOOGLE_OAUTH_CLIENT_ID,
+    process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+    process.env.GOOGLE_OAUTH_REDIRECT_URL
+  )
+
+  client.setCredentials({ access_token: token.data[0].token })
+
+  return client
+}
