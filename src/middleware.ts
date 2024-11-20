6a8d74f1153c0d835103f8bd2b4f57f6473aadbf
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-11-20 21:43:55 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,9 @@ const isPublicRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
-  }
+  // if (!isPublicRoute(req)) {
+    // await auth.protect()
+  // }
 })
 
 export const config = {
